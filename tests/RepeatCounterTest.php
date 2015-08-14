7b99253b6a8f93a5ec1a3a066f04a61d1a5ad884
Ben Spenard <bdspen@gmail.com>
2015-08-14 10:46:07 -0700

    add test for capitalization problems.

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -29,5 +29,18 @@
             //Assert
             $this->assertEquals("1", $result);
         }
+        function test_countRepeats_returnOne()
+        {
+            //Arrange
+            $test_RepeatCounter = new RepeatCounter;
+            $input_one = "a";
+            $input_two = "A";
+
+            //Act
+            $result = $test_RepeatCounter->countRepeats($input_one, $input_two);
+
+            //Assert
+            $this->assertEquals("1", $result);
+        }
     }
  ?>
