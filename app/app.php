7e46fa54832ddc414545a2054377aa1aaf86b32a
Ben Spenard <bdspen@gmail.com>
2015-08-14 12:25:49 -0700

    add app.php index.php and view files

---
+++ app/app.php
@@ -0,0 +1,19 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/RepeatCounter.php";
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('repeat_counter.html.twig');
+    });
+    $app->get("/results", function() use ($app){
+        $results = new RepeatCounter;
+        $outcome = $results->countRepeats($_GET['search_word'], $_GET['sentence']);
+        return $app['twig']->render('results.html.twig', array('result' => $outcome));
+    });
+    return $app;
+?>
