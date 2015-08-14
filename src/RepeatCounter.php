c89b9e0874a04a45156a0e1e4a4a55631c86987a
Ben Spenard <bdspen@gmail.com>
2015-08-14 10:56:16 -0700

    edit method to search within a sentence and find a match for one word

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -6,8 +6,9 @@
         {
             $keyword = strtolower($input_one);
             $string_to_search = strtolower($input_two);
+            $exp_string_to_search = explode(" ", $string_to_search);
 
-            if ($keyword == $string_to_search)
+            if (in_array($keyword, $exp_string_to_search))
             {
                 $output = "1";
             }
